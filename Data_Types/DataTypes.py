bafc4c0be54b18cb4f3b2403aaa82470c76ae12c
Martin-njau <matharamartin69@gmail.com>
2024-02-29 07:15:15 +0300

    first commit

---
+++ Data_Types/DataTypes.py
@@ -0,0 +1,5 @@
+list = ['abcd', 786, 2.23, 'John', 70.2]
+tinylist = [123, 'John']
+print(tinylist)
+print(list[2:3])
+
---
+++ Data_Types/User_Input/user_input.py
@@ -0,0 +1,6 @@
+#Ask for user input
+name= input("What is your name?")
+age= input("How old are you?")
+location= input("Where are youn from?")
+
+print("Hello" + name + ",you are" + age + " years old and live in " + location + ".")
